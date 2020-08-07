ec8b8de1083b085815ace0d4ed03b3fcfbbd09c1
Manuele Sapia <manuele.sapia.comprend@outlook.com>
2020-08-07 07:14:12 +0100

    module 4

---
+++ module4-solution/js/script.js
@@ -0,0 +1,36 @@
+// var array = new Array();
+// array[0] = "Manuel";
+// array[1] = 2;
+// array[2] = function (name) {
+//     console.log("hello" + name);
+// };
+// array[3] = {
+//     course: "html css js"
+// };
+
+// console.log(array);
+
+
+// var names = ["dav", "manuel", "david"];
+// //console.log(names);
+
+// for (var i = 0; i < names.length; i++) {
+//     console.log("hello " + names[i]);
+// }
+
+var names2 = ["jack", "manuel", "david"];
+
+// var myObj = {
+//     name: "manuel",
+//     course: "gfgfgg css js",
+//     platform: "online"
+// }
+
+// for (var prop in myObj) {
+//     console.log(prop + ": " + myObj[prop]);
+// }
+
+
+for (var name in names2) {
+    console.log("hello " + names2[name]);
+}
